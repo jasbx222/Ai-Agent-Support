c2c4f1ffb0eb5e2b4904ab541db71ea7a0e9c467
jassimbandtech <jassimbandtech@gmail.com>
2026-04-18 12:43:27 +0300

    add tools

--- database/migrations/2026_04_17_212428_update_role_enum_in_users_table.php
+++ database/migrations/2026_04_17_212428_update_role_enum_in_users_table.php
@@ -8,20 +8,33 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
- public function up(): void
+public function up(): void
 {
+    // 1. حذف الـ default القديم
+    DB::statement("ALTER TABLE users ALTER COLUMN role DROP DEFAULT");
+
+    // 2. إنشاء enum
     DB::statement("CREATE TYPE user_role AS ENUM ('user', 'admin', 'employee')");
+
+    // 3. تحويل العمود
     DB::statement("ALTER TABLE users ALTER COLUMN role TYPE user_role USING role::text::user_role");
+
+    // 4. إضافة default جديد
     DB::statement("ALTER TABLE users ALTER COLUMN role SET DEFAULT 'user'");
 }
-
     /**
      * Reverse the migrations.
      */
 public function down(): void
 {
+    DB::statement("ALTER TABLE users ALTER COLUMN role DROP DEFAULT");
+
     DB::statement("CREATE TYPE user_role_old AS ENUM ('user', 'admin')");
+
     DB::statement("ALTER TABLE users ALTER COLUMN role TYPE user_role_old USING role::text::user_role_old");
+
+    DB::statement("ALTER TABLE users ALTER COLUMN role SET DEFAULT 'user'");
+
     DB::statement("DROP TYPE user_role");
 }
 };
