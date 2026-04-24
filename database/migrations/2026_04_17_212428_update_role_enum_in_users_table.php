06452ac22195421653ba6628d538549dad1e77fb
jassimbandtech <jassimbandtech@gmail.com>
2026-04-24 16:28:41 +0300

    add notifictions

--- database/migrations/2026_04_17_212428_update_role_enum_in_users_table.php
+++ database/migrations/2026_04_17_212428_update_role_enum_in_users_table.php
@@ -7,37 +7,37 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // DB::statement("ALTER TABLE users ALTER COLUMN role DROP DEFAULT");
+        DB::statement("ALTER TABLE users ALTER COLUMN role DROP DEFAULT");
 
-        // DB::statement("DROP TYPE IF EXISTS user_role");
+        DB::statement("DROP TYPE IF EXISTS user_role");
 
-        // DB::statement("CREATE TYPE user_role AS ENUM ('user', 'admin', 'employee')");
+        DB::statement("CREATE TYPE user_role AS ENUM ('user', 'admin', 'employee')");
 
-        // DB::statement("
-        //     ALTER TABLE users
-        //     ALTER COLUMN role TYPE user_role
-        //     USING role::text::user_role
-        // ");
+        DB::statement("
+            ALTER TABLE users
+            ALTER COLUMN role TYPE user_role
+            USING role::text::user_role
+        ");
 
-        // DB::statement("ALTER TABLE users ALTER COLUMN role SET DEFAULT 'user'");
+        DB::statement("ALTER TABLE users ALTER COLUMN role SET DEFAULT 'user'");
     }
 
     public function down(): void
     {
-        // DB::statement("ALTER TABLE users ALTER COLUMN role DROP DEFAULT");
+        DB::statement("ALTER TABLE users ALTER COLUMN role DROP DEFAULT");
 
-        // DB::statement("DROP TYPE IF EXISTS user_role_old");
+        DB::statement("DROP TYPE IF EXISTS user_role_old");
 
-        // DB::statement("CREATE TYPE user_role_old AS ENUM ('user', 'admin')");
+        DB::statement("CREATE TYPE user_role_old AS ENUM ('user', 'admin')");
 
-        // DB::statement("
-        //     ALTER TABLE users
-        //     ALTER COLUMN role TYPE user_role_old
-        //     USING role::text::user_role_old
-        // ");
+        DB::statement("
+            ALTER TABLE users
+            ALTER COLUMN role TYPE user_role_old
+            USING role::text::user_role_old
+        ");
 
-        // DB::statement("ALTER TABLE users ALTER COLUMN role SET DEFAULT 'user'");
+        DB::statement("ALTER TABLE users ALTER COLUMN role SET DEFAULT 'user'");
 
-        // DB::statement("DROP TYPE IF EXISTS user_role");
+        DB::statement("DROP TYPE IF EXISTS user_role");
     }
 };
